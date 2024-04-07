fix: improve sigmoid, add softmax and shifted sigmoid

utils.py
@@ -38,7 +38,15 @@ def sqrt(x):
 
 
 def sigmoid(x):
-    return 1 / (1 - np.exp(-x))
+    return 1 / (1 + np.exp(-x))
+
+def softmax(x):
+    shift_x = x - np.max(x, axis=-1, keepdims=True)
+    exp_x = np.exp(shift_x)
+    return exp_x / np.sum(exp_x, axis=-1, keepdims=True)
+
+def shifted_sigmoid(x):
+    return sigmoid(x) - sigmoid(0)
 
 
 def dist_to_matrix(dist):